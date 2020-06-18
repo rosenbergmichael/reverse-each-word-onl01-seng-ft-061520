a052562066e20ec09ec044ecf196445bd4e5291a
Michael Rosenberg <rosenbergmj1@gmail.com>
2020-06-18 01:36:43 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(sentence)
   my_sentence = []
   sentence.each do |sentence|
-    my_sentence << "Hello there, and how are you?"
+    my_sentence << string.reverse
 end
 my_sentence 
 end
