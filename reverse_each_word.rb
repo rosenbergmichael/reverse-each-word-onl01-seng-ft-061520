52af5dfb517e0de59fdabb1a0ae8769a4fd80747
Michael Rosenberg <rosenbergmj1@gmail.com>
2020-06-18 01:51:07 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,19 @@
+def reverse_each_word(sentence)
+  sentence = sentence.split(" ")
+  my_sentence = []
+  sentence.each do |sentence|
+    my_sentence << sentence.reverse
+end
+my_sentence.join(" ")
+end
+
+def reverse_each_word(sentence)
+  sentence=sentence.split (" ")
+  my_sentence=[]
+  sentence.collect do |sentence|
+    my_sentence<<sentence.reverse 
+end
+  my_sentence.join(" ")
+end
+
+  
